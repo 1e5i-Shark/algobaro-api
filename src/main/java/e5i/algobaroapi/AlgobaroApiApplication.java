7ffd5e179ca1c7a453ae993bedc798a8b140be2d
hyoguoo <hyoguoo@gmail.com>
2024-02-11 20:12:32 +0900

    init: init project

---
+++ src/main/java/e5i/algobaroapi/AlgobaroApiApplication.java
@@ -0,0 +1,12 @@
+package e5i.algobaroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgobaroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgobaroApiApplication.class, args);
+    }
+}
---
+++ src/test/java/e5i/algobaroapi/AlgobaroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package e5i.algobaroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgobaroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
