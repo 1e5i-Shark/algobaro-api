03c6d8b3e4679578141b1b7ce14e2fd415bef3c0
hyoguoo <hyoguoo@gmail.com>
2024-02-12 01:03:13 +0900

    feat: Member 생성 시 패스워드 정책 검증 및 인코드를 위한 Util 클래스 추가
    Co-authored-by: kimnamgyu <lethargic.hood@gmail.com>

---
+++ src/main/java/ei/algobaroapi/domain/member/util/PasswordUtil.java
@@ -0,0 +1,32 @@
+package ei.algobaroapi.domain.member.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@RequiredArgsConstructor
+public class PasswordUtil {
+
+    private static final int PASSWORD_LENGTH = 8;
+    private static final String SPECIAL_CHARACTERS_REGEX = ".*[!@#$%^&*()].*";
+
+    private final PasswordEncoder passwordEncoder;
+
+    public String validateAndEncryptPassword(String rawPassword) {
+        if (!isValidPassword(rawPassword)) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.length() >= PASSWORD_LENGTH && password.matches(SPECIAL_CHARACTERS_REGEX);
+    }
+
+    public boolean isPasswordMatch(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
