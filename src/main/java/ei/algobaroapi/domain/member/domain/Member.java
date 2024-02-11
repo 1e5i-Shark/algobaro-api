0a880f2dd90a5ff69da5408540fc5c465178125d
hyoguoo <hyoguoo@gmail.com>
2024-02-12 01:01:07 +0900

    feat: 필수 정보만 포함한 Member Entity 추가
    Co-authored-by: kimnamgyu <lethargic.hood@gmail.com>

---
+++ src/main/java/ei/algobaroapi/domain/member/domain/Member.java
@@ -0,0 +1,74 @@
+package ei.algobaroapi.domain.member.domain;
+
+import ei.algobaroapi.domain.member.domain.vo.EmailVo;
+import ei.algobaroapi.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity implements UserDetails {
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    private final List<String> roles = new ArrayList<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private EmailVo email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Builder
+    public Member(String email, String password) {
+        this.email = new EmailVo(email);
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
