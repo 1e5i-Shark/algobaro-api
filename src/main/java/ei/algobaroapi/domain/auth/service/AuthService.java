e7842342b923f0a828eeff459c25d25e03b54967
hyoguoo <hyoguoo@gmail.com>
2024-02-13 12:24:13 +0900

    refactor: AuthService에서 MemberService 의존하도록 변경
    Co-authored-by: kimnamgyu <lethargic.hood@gmail.com>

--- src/main/java/ei/algobaroapi/domain/auth/service/AuthService.java
+++ src/main/java/ei/algobaroapi/domain/auth/service/AuthService.java
@@ -1,14 +1,12 @@
 package ei.algobaroapi.domain.auth.service;
 
 import ei.algobaroapi.domain.member.domain.Member;
-import ei.algobaroapi.domain.member.domain.MemberRepository;
-import ei.algobaroapi.domain.member.domain.vo.EmailVo;
 import ei.algobaroapi.domain.auth.dto.MemberSignUpRequest;
 import ei.algobaroapi.domain.auth.dto.MemberSignInRequest;
 import ei.algobaroapi.domain.auth.exception.AuthErrorCode;
-import ei.algobaroapi.domain.auth.exception.umm.MemberFoundException;
 import ei.algobaroapi.domain.auth.exception.umm.MemberPasswordException;
 import ei.algobaroapi.domain.auth.util.PasswordUtil;
+import ei.algobaroapi.domain.member.service.MemberService;
 import ei.algobaroapi.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,20 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final JwtProvider jwtProvider;
     private final PasswordUtil passwordUtil;
 
     @Transactional
     public void signUp(MemberSignUpRequest request) {
         String encryptPassword = passwordUtil.validateAndEncryptPassword(request.getPassword());
-        memberRepository.save(request.toEntity(encryptPassword));
+        memberService.addMember(request.toEntity(encryptPassword));
     }
 
     @Transactional
     public String signIn(MemberSignInRequest request) {
-        Member member = memberRepository.findByEmailAndDeletedAtIsNull(new EmailVo(request.getEmail()))
-                .orElseThrow(() -> MemberFoundException.of(AuthErrorCode.EMAIL_NOT_FOUND));
+        Member member = memberService.getMemberByEmail(request.getEmail());
         if (!passwordUtil.isPasswordMatch(request.getPassword(), member.getPassword())) {
             throw MemberPasswordException.of(AuthErrorCode.PASSWORD_NOT_MATCH);
         }
