7ecf006d63f2d30918a058936e4ee828e6bf9239
hyoguoo <hyoguoo@gmail.com>
2024-02-15 12:28:24 +0900

    test: DB Connection 테스트 원복

--- src/test/java/ei/algobaroapi/AlgobaroApiApplicationTest.java
+++ src/test/java/ei/algobaroapi/AlgobaroApiApplicationTest.java
@@ -1,30 +1,28 @@
 package ei.algobaroapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
-import ei.algobaroapi.domain.member.domain.Member;
-import ei.algobaroapi.domain.member.domain.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class AlgobaroApiApplicationTest {
 
     @Autowired
-    private MemberRepository memberRepository;
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     @DisplayName("DB Connection 테스트")
     void testDatabaseConnection() {
-        Member newMember = Member.builder()
-                .email("test@test.com")
-                .password("test")
-                .build();
-
-        Member savedMember = memberRepository.save(newMember);
-
-        assertThat(savedMember).isNotNull();
+        try {
+            Integer test = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            assertThat(test).isEqualTo(1);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 }
