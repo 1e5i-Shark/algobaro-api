d18c64f5798e460219021615c33c4ffd1208ea8b
hyoguoo <hyoguoo@gmail.com>
2024-02-16 10:28:17 +0900

    feat: 접근 권한 관련 Access Denied Handler 추가
    Co-authored-by: kimnamgyu <lethargic.hood@gmail.com>

--- src/main/java/ei/algobaroapi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/ei/algobaroapi/global/exception/GlobalExceptionHandler.java
@@ -4,12 +4,21 @@ import ei.algobaroapi.global.response.message.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> catchAccessDeniedException(AccessDeniedException e) {
+        log.warn(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ErrorResponse.of("E00201", "Access Denied"));
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> catchMemberPasswordException(RuntimeException e) {
