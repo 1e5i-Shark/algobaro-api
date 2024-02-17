90b64b0bca138ad41b8803708bd472e6a9e23441
hyoguoo <hyoguoo@gmail.com>
2024-02-18 05:27:44 +0900

    docs: Member / Problem / Compile / Solve 도메인 컨트롤러 Swagger 문서 작성

---
+++ src/main/java/ei/algobaroapi/domain/compile/controller/CompileController.java
@@ -0,0 +1,31 @@
+package ei.algobaroapi.domain.compile.controller;
+
+import ei.algobaroapi.domain.compile.dto.request.CompileExecutionRequest;
+import ei.algobaroapi.domain.compile.dto.response.CompileExecutionResponse;
+import ei.algobaroapi.domain.member.domain.Member;
+import ei.algobaroapi.global.config.swaggerdoc.CompileControllerDoc;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class CompileController implements CompileControllerDoc {
+
+    @Override
+    @PostMapping("/compile")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompileExecutionResponse compileCode(
+            @AuthenticationPrincipal Member member,
+            @RequestBody @Valid CompileExecutionRequest request
+    ) {
+        return null;
+    }
+}
