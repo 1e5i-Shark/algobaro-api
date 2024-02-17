3b87ece04d0db717eb144f2a238adecd57bac299
hyoguoo <hyoguoo@gmail.com>
2024-02-18 04:51:36 +0900

    feat: 문제 풀이 히스토리 Entity 추가

---
+++ src/main/java/ei/algobaroapi/domain/solve/domain/SolveHistory.java
@@ -0,0 +1,65 @@
+package ei.algobaroapi.domain.solve.domain;
+
+import ei.algobaroapi.domain.member.domain.Member;
+import ei.algobaroapi.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "solve_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SolveHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "room_uuid", nullable = false)
+    private String roomUuid;
+
+    @Column(name = "input_code", nullable = false, columnDefinition = "TEXT")
+    private String inputCode;
+
+    @Column(name = "code_language", nullable = false)
+    private String codeLanguage;
+
+    @Column(name = "start_at", nullable = false)
+    private String startAt;
+
+    @Column(name = "end_at", nullable = false)
+    private String endAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "solve_status", nullable = false)
+    private SolveStatus solveStatus;
+
+    @Column(name = "problem_link", nullable = false)
+    private String problemLink;
+
+    @Column(name = "problem_name", nullable = false)
+    private String problemName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "problem_platform", nullable = false)
+    private ProblemPlatform problemPlatform;
+
+    @Column(name = "problem_level", nullable = false)
+    private String problemLevel;
+}
