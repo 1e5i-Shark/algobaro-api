9fa5d9d734ab1aa2eda674b63ddec4a087b3afbe
hyoguoo <hyoguoo@gmail.com>
2024-02-18 04:50:43 +0900

    feat: Problem Service Interface 설계
    - getProblemInfoHtml: 문제 정보 html 조회 메서드
    - getProblemTestCases: 문제 테스트 케이스 리스트 조회 메서드

---
+++ src/main/java/ei/algobaroapi/domain/problem/dto/response/ProblemHtmlResponse.java
@@ -0,0 +1,17 @@
+package ei.algobaroapi.domain.problem.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Schema(description = "문제 정보")
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ProblemHtmlResponse {
+
+    @Schema(description = "문제 정보 html", example = "<h1>문제 정보</h1>")
+    private final String problemInfoHtml;
+
+    public static ProblemHtmlResponse of(String problemInfoHtml) {
+        return new ProblemHtmlResponse(problemInfoHtml);
+    }
+}
