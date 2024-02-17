b90abc9ac1ea98f83c1fb2aeccbd77de0e6bb894
hyoguoo <hyoguoo@gmail.com>
2024-02-18 04:44:45 +0900

    feat: Member Service 인터페이스 생성 및 필요 메서드 설계
    - getMemberByEmail
    - addMember
    - getMemberById
    - getMemberDetailById: 내 정보 조회 시 사용
    - updateMemberDetail: 내 정보 업데이트 시 사용

--- src/main/java/ei/algobaroapi/domain/member/service/MemberService.java
+++ src/main/java/ei/algobaroapi/domain/member/service/MemberService.java
@@ -1,28 +1,18 @@
 package ei.algobaroapi.domain.member.service;
 
 import ei.algobaroapi.domain.member.domain.Member;
-import ei.algobaroapi.domain.member.domain.MemberRepository;
-import ei.algobaroapi.domain.member.domain.vo.EmailVo;
-import ei.algobaroapi.domain.member.exception.MemberFoundException;
-import ei.algobaroapi.domain.member.exception.common.MemberErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import ei.algobaroapi.domain.member.dto.response.MemberDetailResponse;
+import ei.algobaroapi.domain.member.dto.request.MemberDetailUpdateRequest;
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class MemberService {
+public interface MemberService {
 
-    private final MemberRepository memberRepository;
+    Member getMemberByEmail(String email);
 
-    public Member getMemberByEmail(String email) {
-        return memberRepository.findByEmailAndDeletedAtIsNull(new EmailVo(email))
-                .orElseThrow(() -> MemberFoundException.of(MemberErrorCode.EMAIL_NOT_FOUND));
-    }
+    Member addMember(Member member);
 
-    @Transactional
-    public Member addMember(Member member) {
-        return memberRepository.save(member);
-    }
+    Member getMemberById(Long id);
+
+    MemberDetailResponse getMemberDetailById(Long id);
+
+    void updateMemberDetail(Long id, MemberDetailUpdateRequest request);
 }
