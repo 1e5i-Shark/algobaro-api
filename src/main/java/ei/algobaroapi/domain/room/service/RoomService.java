4f61a50cf0bfc3c86e87294536a666e0ff9c9e09
kimnamgyu <lethargic.hood@gmail.com>
2024-02-18 17:35:04 +0900

    feat #12: Room Service 인터페이스 생성
    - getAllRooms: 모든 방 정보 조회
    - createRoom: 방 생성
    - updateRoomByRoomId: 방 수정 by 방 번호
    - startCodingTest: 코드 풀이 시작
    - getSubmitCodesByRoomId: 제출 코드 조회 by 방 번호

---
+++ src/main/java/ei/algobaroapi/domain/room/service/RoomService.java
@@ -0,0 +1,20 @@
+package ei.algobaroapi.domain.room.service;
+
+import ei.algobaroapi.domain.room.dto.request.RoomCreateRequestDto;
+import ei.algobaroapi.domain.room.dto.request.RoomUpdateRequestDto;
+import ei.algobaroapi.domain.room.dto.response.RoomDetailResponseDto;
+import ei.algobaroapi.domain.room.dto.response.RoomSubmitCodeResponseDto;
+import java.util.List;
+
+public interface RoomService {
+
+    List<RoomDetailResponseDto> getAllRooms();
+
+    RoomDetailResponseDto createRoom(RoomCreateRequestDto roomCreateRequestDto);
+
+    RoomDetailResponseDto updateRoomByRoomId(Long roomId, RoomUpdateRequestDto roomUpdateRequestDto);
+
+    void startCodingTest(Long roomId);
+
+    List<RoomSubmitCodeResponseDto> getSubmitCodesByRoomId(Long roomId);
+}
