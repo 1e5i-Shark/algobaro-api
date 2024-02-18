bf4189f16bb2f81ff4ef6246b39bbdffb86a31fe
kimnamgyu <lethargic.hood@gmail.com>
2024-02-18 17:40:05 +0900

    feat #12: RoomMember 엔티티 구성

---
+++ src/main/java/ei/algobaroapi/domain/room_member/domain/RoomMember.java
@@ -0,0 +1,48 @@
+package ei.algobaroapi.domain.room_member.domain;
+
+import ei.algobaroapi.domain.member.domain.Member;
+import ei.algobaroapi.domain.room.domain.Room;
+import ei.algobaroapi.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "room_members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoomMember extends BaseEntity {
+
+    @Id
+    @Column(name = "room_members_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "members_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "room_members_role", nullable = false)
+    private RoomMemberRole roomMemberRole;
+
+    @Column(name = "is_ready")
+    private boolean isReady;
+
+    @Column(name = "submit_code")
+    private String submitCode;
+}
