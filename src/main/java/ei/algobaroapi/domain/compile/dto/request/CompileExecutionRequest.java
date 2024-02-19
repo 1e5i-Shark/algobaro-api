d099184ebd2425b669b9dd06b30dae606e459a6e
hyoguoo <hyoguoo@gmail.com>
2024-02-19 11:44:53 +0900

    docs: 예시 코드 올바른 코드로 변경

--- src/main/java/ei/algobaroapi/domain/compile/dto/request/CompileExecutionRequest.java
+++ src/main/java/ei/algobaroapi/domain/compile/dto/request/CompileExecutionRequest.java
@@ -15,6 +15,6 @@ public class CompileExecutionRequest {
     @Schema(description = "코드 실행 입력 값", example = "1 2")
     private String input;
 
-    @Schema(description = "코드 실행 코드", example = "import java.io.BufferedReader;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(\" \");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}")
+    @Schema(description = "코드 실행 코드", example = "import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(\" \");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}")
     private String code;
 }
