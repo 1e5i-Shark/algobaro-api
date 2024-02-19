7d7afbc7a6a680f05dea6d40e919a9b27256bfce
hyoguoo <hyoguoo@gmail.com>
2024-02-19 12:05:35 +0900

    feat: 컴파일 요청 시 Request 값 Not Null 설정

--- src/main/java/ei/algobaroapi/domain/compile/dto/request/CompileExecutionRequest.java
+++ src/main/java/ei/algobaroapi/domain/compile/dto/request/CompileExecutionRequest.java
@@ -1,6 +1,7 @@
 package ei.algobaroapi.domain.compile.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,11 +11,14 @@ import lombok.Getter;
 public class CompileExecutionRequest {
 
     @Schema(description = "코드 실행 언어", example = "java")
+    @NotNull
     private String language;
 
     @Schema(description = "코드 실행 입력 값", example = "1 2")
+    @NotNull
     private String input;
 
     @Schema(description = "코드 실행 코드", example = "import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(\" \");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}")
+    @NotNull
     private String code;
 }
