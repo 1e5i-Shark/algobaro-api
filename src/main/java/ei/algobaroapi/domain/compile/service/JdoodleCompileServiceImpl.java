6b9c83df4ec50cee2f164a344aa2aee3d36aa225
hyoguoo <hyoguoo@gmail.com>
2024-02-19 12:04:56 +0900

    feat: jdoodle 컴파일 요청 서비스 로직 구현

--- src/main/java/ei/algobaroapi/domain/compile/service/JdoodleCompileServiceImpl.java
+++ src/main/java/ei/algobaroapi/domain/compile/service/JdoodleCompileServiceImpl.java
@@ -1,8 +1,12 @@
 package ei.algobaroapi.domain.compile.service;
 
 import ei.algobaroapi.domain.compile.dto.request.CompileExecutionRequest;
+import ei.algobaroapi.domain.compile.dto.request.JdoodleCompileRequest;
 import ei.algobaroapi.domain.compile.dto.response.CompileExecutionResponse;
+import ei.algobaroapi.domain.compile.dto.response.JdoodleCompileResponse;
+import ei.algobaroapi.global.util.HttpUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +15,34 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class JdoodleCompileServiceImpl implements CompileService {
 
+    private final HttpUtil httpUtil;
+    @Value("${spring.myapp.jdoodle.client-id}")
+    private String clientId;
+    @Value("${spring.myapp.jdoodle.client-secret}")
+    private String clientSecret;
+    @Value("${spring.myapp.jdoodle.api-url}")
+    private String jdoodleApiUrl;
+
     @Override
     public CompileExecutionResponse executeCode(CompileExecutionRequest request) {
-        return null;
+        JdoodleCompileResponse jdoodleCompileResponse = sendJdoodleCompileRequest(
+                JdoodleCompileRequest.builder()
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .stdin(request.getInput())
+                        .script(request.getCode())
+                        .language(request.getLanguage())
+                        .build()
+        );
+
+        return CompileExecutionResponse.of(jdoodleCompileResponse.getOutput());
+    }
+
+    private JdoodleCompileResponse sendJdoodleCompileRequest(JdoodleCompileRequest request) {
+        return httpUtil.requestPostWithBasicAuthorization(
+                jdoodleApiUrl,
+                request,
+                JdoodleCompileResponse.class
+        );
     }
 }
