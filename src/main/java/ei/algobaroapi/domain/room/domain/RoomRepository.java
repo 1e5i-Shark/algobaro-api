6badafb3d361235e56cdbfae7b390f0da3ad58c9
kimnamgyu <lethargic.hood@gmail.com>
2024-02-20 13:23:19 +0900

    feat #22: CRU를 위한 Repository 생성

---
+++ src/main/java/ei/algobaroapi/domain/room/domain/RoomRepository.java
@@ -0,0 +1,9 @@
+package ei.algobaroapi.domain.room.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
