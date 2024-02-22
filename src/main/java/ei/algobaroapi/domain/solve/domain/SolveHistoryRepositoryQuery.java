4a1f7211bdf6d103a9264133a83b42724b836149
hyoguoo <hyoguoo@gmail.com>
2024-02-22 17:01:44 +0900

    feat #25: 풀이 히스토리 조회 Repository 기능 구현

---
+++ src/main/java/ei/algobaroapi/domain/solve/domain/SolveHistoryRepositoryQuery.java
@@ -0,0 +1,10 @@
+package ei.algobaroapi.domain.solve.domain;
+
+import ei.algobaroapi.domain.solve.dto.request.SolveHistoryListFindRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface SolveHistoryRepositoryQuery {
+
+    Page<SolveHistory> findListPage(SolveHistoryListFindRequest request, Pageable pageable, Long memberId);
+}
