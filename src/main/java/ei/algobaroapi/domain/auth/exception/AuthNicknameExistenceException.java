20bbe808715d328409e218e9f84b362dec56d646
kimnamgyu <lethargic.hood@gmail.com>
2024-02-22 16:29:53 +0900

    feat #29: 닉네임 중복에 대한 예외처리 추가

--- src/main/java/ei/algobaroapi/domain/auth/exception/AuthNicknameExistenceException.java
+++ src/main/java/ei/algobaroapi/domain/auth/exception/AuthNicknameExistenceException.java
@@ -4,17 +4,17 @@ import ei.algobaroapi.domain.auth.exception.common.AuthErrorCode;
 import lombok.Getter;
 
 @Getter
-public class AuthPasswordConfirmationException extends RuntimeException {
+public class AuthNicknameExistenceException extends RuntimeException {
 
     private final String errorCode;
     private final String errorMessage;
 
-    private AuthPasswordConfirmationException(AuthErrorCode errorCode) {
+    private AuthNicknameExistenceException(AuthErrorCode errorCode) {
         this.errorCode = errorCode.getErrorCode();
         this.errorMessage = errorCode.getErrorMessage();
     }
 
-    public static AuthPasswordConfirmationException of(AuthErrorCode errorCode) {
-        return new AuthPasswordConfirmationException(errorCode);
+    public static AuthNicknameExistenceException of(AuthErrorCode errorCode) {
+        return new AuthNicknameExistenceException(errorCode);
     }
 }
