0b97a0f4bd5b00dda8cdbd1e78980928fea6a2b4
hyoguoo <hyoguoo@gmail.com>
2024-02-22 17:44:13 +0900

    feat #25: 풀이 히스토리 FETCH JOIN 조회 메서드 추가

--- src/main/java/ei/algobaroapi/domain/solve/domain/SolveHistoryRepository.java
+++ src/main/java/ei/algobaroapi/domain/solve/domain/SolveHistoryRepository.java
@@ -1,6 +1,8 @@
 package ei.algobaroapi.domain.solve.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,4 +10,6 @@ public interface SolveHistoryRepository extends
         JpaRepository<SolveHistory, Long>,
         SolveHistoryRepositoryQuery {
 
+    @Query("select s from SolveHistory s join fetch s.member where s.id = :solveId")
+    Optional<SolveHistory> findByIdWithMember(Long solveId);
 }
