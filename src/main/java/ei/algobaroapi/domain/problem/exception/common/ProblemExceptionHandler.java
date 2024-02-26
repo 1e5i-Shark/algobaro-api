7715c090ccb328e019f4a83b3a087391faafacb4
kimnamgyu <lethargic.hood@gmail.com>
2024-02-26 11:57:00 +0900

    feat #38: 크롤링 실패 시 발생할 예외 처리

---
+++ src/main/java/ei/algobaroapi/domain/problem/exception/common/ProblemExceptionHandler.java
@@ -0,0 +1,21 @@
+package ei.algobaroapi.domain.problem.exception.common;
+
+import ei.algobaroapi.domain.problem.exception.CrawlingAccessException;
+import ei.algobaroapi.global.response.message.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ProblemExceptionHandler {
+
+    @ExceptionHandler(CrawlingAccessException.class)
+    public ResponseEntity<ErrorResponse> catchCrawlingAccessException(CrawlingAccessException e) {
+        log.warn(e.getErrorMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.of(e.getErrorCode(), e.getErrorMessage()));
+    }
+}
