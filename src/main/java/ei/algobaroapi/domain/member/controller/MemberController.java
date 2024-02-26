3b9964d6ea14731bdffecc1b96620d6c3bdbe14d
hyoguoo <hyoguoo@gmail.com>
2024-02-26 11:17:23 +0900

    feat #28: 내 정보 수정 API 구현

--- src/main/java/ei/algobaroapi/domain/member/controller/MemberController.java
+++ src/main/java/ei/algobaroapi/domain/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import ei.algobaroapi.domain.member.service.MemberService;
 import ei.algobaroapi.global.config.swaggerdoc.MemberControllerDoc;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,16 +24,18 @@ public class MemberController implements MemberControllerDoc {
 
     @Override
     @GetMapping("/members/my")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public MemberDetailResponse getMyInfo(@AuthenticationPrincipal Member member) {
         return memberService.getMemberDetailById(member.getId());
     }
 
     @Override
     @PatchMapping("/members/my")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public void updateMemberInfo(
             @AuthenticationPrincipal Member member,
             @RequestBody @Valid MemberDetailUpdateRequest memberUpdateRequest
     ) {
-        //
+        memberService.updateMemberDetail(member.getId(), memberUpdateRequest);
     }
 }
