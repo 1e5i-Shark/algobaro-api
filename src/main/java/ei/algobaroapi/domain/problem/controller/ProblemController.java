55899795982672ff76476b4fbf7ab9ec8c716de2
kimnamgyu <lethargic.hood@gmail.com>
2024-02-26 11:55:58 +0900

    feat #38: 크롤링 로직 구현

--- src/main/java/ei/algobaroapi/domain/problem/controller/ProblemController.java
+++ src/main/java/ei/algobaroapi/domain/problem/controller/ProblemController.java
@@ -2,6 +2,7 @@ package ei.algobaroapi.domain.problem.controller;
 
 import ei.algobaroapi.domain.problem.dto.request.ProblemFindRequest;
 import ei.algobaroapi.domain.problem.dto.response.ProblemHtmlResponse;
+import ei.algobaroapi.domain.problem.service.BojProblemServiceImpl;
 import ei.algobaroapi.global.config.swaggerdoc.ProblemControllerDoc;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ProblemController implements ProblemControllerDoc {
 
+    private final BojProblemServiceImpl bojProblemService;
+
     @Override
     @GetMapping("/problems/html")
     public ProblemHtmlResponse getProblemHtml(@RequestBody @Valid ProblemFindRequest request) {
-        return null;
+        return bojProblemService.getProblemInfoHtml(request);
     }
 }
--- src/main/java/ei/algobaroapi/domain/problem/service/BojProblemServiceImpl.java
+++ src/main/java/ei/algobaroapi/domain/problem/service/BojProblemServiceImpl.java
@@ -4,10 +4,14 @@ import ei.algobaroapi.domain.problem.dto.request.ProblemFindRequest;
 import ei.algobaroapi.domain.problem.dto.request.ProblemSolveRequest;
 import ei.algobaroapi.domain.problem.dto.response.ProblemHtmlResponse;
 import ei.algobaroapi.domain.problem.dto.response.ProblemTestCaseResponse;
+import ei.algobaroapi.domain.problem.exception.CrawlingAccessException;
+import ei.algobaroapi.domain.problem.exception.common.ProblemErrorCode;
 import ei.algobaroapi.domain.solve.domain.SolveStatus;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +22,12 @@ public class BojProblemServiceImpl implements ProblemService {
 
     @Override
     public ProblemHtmlResponse getProblemInfoHtml(ProblemFindRequest request) {
-        return null;
+        try {
+            return ProblemHtmlResponse.of(
+                    Jsoup.connect(request.getProblemLink()).get().outerHtml());
+        } catch (IOException e) {
+            throw CrawlingAccessException.of(ProblemErrorCode.CRAWLING_NOT_ACCESS);
+        }
     }
 
     @Override
