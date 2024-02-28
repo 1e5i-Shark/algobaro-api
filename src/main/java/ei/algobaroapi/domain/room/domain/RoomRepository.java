30a11bd6af153d0c6231af648934f1c2987040ab
kimnamgyu <lethargic.hood@gmail.com>
2024-02-28 13:16:00 +0900

    mod #41: short UUID를 통해 방 조회할 수 있도록 Query 변경

--- src/main/java/ei/algobaroapi/domain/room/domain/RoomRepository.java
+++ src/main/java/ei/algobaroapi/domain/room/domain/RoomRepository.java
@@ -1,7 +1,6 @@
 package ei.algobaroapi.domain.room.domain;
 
 import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    @Query("SELECT r from Room r where r.roomUuid = :roomUuid")
-    Optional<Room> findByRoomUuidWithRoomMember(UUID roomUuid); // TODO: 방 참여 기능 구현 후 쿼리 수정
+    @Query("SELECT r FROM Room r WHERE r.roomUuid LIKE CONCAT(:roomShortUuid, '%')")
+    Optional<Room> findByRoomUuidStartingWith(String roomShortUuid); // TODO: 방 참여 기능 구현 후 쿼리 수정
 }
