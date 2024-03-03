4f986374f5ff8180ebd92b8f9fee290ffc263660
kimnamgyu <lethargic.hood@gmail.com>
2024-03-04 00:57:30 +0900

    feat #31: RoomMember 서비스 메서드 추가
    - 생성 / 참여 메서드 구분
    - 참여 인원 정보 메서드 추가
    - 반환 값 변경 - 방 참여 인원 정보

--- src/main/java/ei/algobaroapi/domain/room_member/service/RoomMemberService.java
+++ src/main/java/ei/algobaroapi/domain/room_member/service/RoomMemberService.java
@@ -1,12 +1,19 @@
 package ei.algobaroapi.domain.room_member.service;
 
 import ei.algobaroapi.domain.member.domain.Member;
+import ei.algobaroapi.domain.room.domain.Room;
 import ei.algobaroapi.domain.room_member.domain.RoomMember;
 import ei.algobaroapi.domain.room_member.dto.response.RoomHostResponseDto;
+import ei.algobaroapi.domain.room_member.dto.response.RoomMemberResponseDto;
+import java.util.List;
 
 public interface RoomMemberService {
 
-    void joinRoomByRoomId(Long roomId, Member member);
+    List<RoomMemberResponseDto> createRoomByRoomId(Room room, Member member);
+
+    List<RoomMemberResponseDto> joinRoomByRoomId(Long roomId, Member member);
+
+    List<RoomMemberResponseDto> getRoomMembersByRoomId(Long roomId);
 
     RoomHostResponseDto changeHostManually(Long hostId, Long organizerId);
 
