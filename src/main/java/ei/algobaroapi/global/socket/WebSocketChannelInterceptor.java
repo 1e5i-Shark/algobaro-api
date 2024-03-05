b4990d5e3f87865eb2be6df8a3f8b2cc8e63aeea
hyoguoo <hyoguoo@gmail.com>
2024-03-05 10:51:38 +0900

    feat #27: 소켓 인증/인가를 위한 WebSocketChannelInterceptor 추가
    CONNECT / SUBSCRIBE에 대해서만 토큰 검사하도록 설정

---
+++ src/main/java/ei/algobaroapi/global/socket/WebSocketChannelInterceptor.java
@@ -0,0 +1,42 @@
+package ei.algobaroapi.global.socket;
+
+import ei.algobaroapi.global.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketChannelInterceptor implements ChannelInterceptor {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if (accessor.getCommand() == StompCommand.CONNECT ||
+                accessor.getCommand() == StompCommand.SUBSCRIBE) {
+            String token = extractToken(accessor);
+            if (!jwtProvider.validateToken(token)) {
+                throw new IllegalArgumentException("Invalid token");
+            }
+        }
+
+        return message;
+    }
+
+    private String extractToken(StompHeaderAccessor accessor) {
+        String authorization = accessor.getFirstNativeHeader(AUTHORIZATION_HEADER);
+        if (authorization == null || !authorization.startsWith(BEARER_PREFIX)) {
+            throw new IllegalArgumentException("Invalid token");
+        }
+        return authorization.substring(BEARER_PREFIX.length());
+    }
+}
