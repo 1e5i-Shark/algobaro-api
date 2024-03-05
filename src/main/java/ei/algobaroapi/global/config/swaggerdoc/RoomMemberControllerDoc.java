4853fff278e4dfade8115c89d3173e72ff2652d4
kimnamgyu <lethargic.hood@gmail.com>
2024-03-05 16:05:57 +0900

    mod #75: 요청 정보 변경 (PathVariable -> RequestBody)

--- src/main/java/ei/algobaroapi/global/config/swaggerdoc/RoomMemberControllerDoc.java
+++ src/main/java/ei/algobaroapi/global/config/swaggerdoc/RoomMemberControllerDoc.java
@@ -2,12 +2,14 @@ package ei.algobaroapi.global.config.swaggerdoc;
 
 import ei.algobaroapi.domain.member.domain.Member;
 import ei.algobaroapi.domain.room_member.domain.RoomMember;
+import ei.algobaroapi.domain.room_member.dto.request.HostChangeRequestDto;
 import ei.algobaroapi.domain.room_member.dto.response.RoomHostResponseDto;
 import ei.algobaroapi.domain.room_member.dto.response.RoomMemberResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "RoomMember", description = "방-회원 관련 API")
 @SuppressWarnings("unused")
@@ -19,7 +21,7 @@ public interface RoomMemberControllerDoc {
 
     @Operation(summary = "방장 수동 변경 - 작업 중", description = "현재 방장이 참여자에게 방장 권한을 위임합니다.")
     @ApiResponse(responseCode = "200", description = "방장 수동 위임에 성공하였습니다.")
-    RoomHostResponseDto changeHostManually(Long hostId, Long organizerId);
+    RoomHostResponseDto changeHostManually(HostChangeRequestDto hostChangeRequestDto);
 
     @Operation(summary = "방장 자동 변경 - 작업 중", description = "현재 방장이 방을 나갔을 경우, 방에 참여한 순으로 방장을 새로 위임합니다.")
     @ApiResponse(responseCode = "200", description = "방장 자동 위임에 성공하였습니다.")
