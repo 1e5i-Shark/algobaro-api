9ff60655a93abe4b65f89df324c76296f1065b32
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 18:00:30 +0900

    feat #71: 방 인원이 최대 인원에 도달 예외 추가

--- src/main/java/ei/algobaroapi/domain/room_member/exception/common/RoomMemberErrorCode.java
+++ src/main/java/ei/algobaroapi/domain/room_member/exception/common/RoomMemberErrorCode.java
@@ -10,7 +10,8 @@ public enum RoomMemberErrorCode {
     ROOM_MEMBER_ERROR_CODE("E05301", "해당 방에 멤버를 찾지 못했습니다."),
     ROOM_MEMBER_IS_NOT_HOST("E05302", "방장 권한을 위임할 수 있는 권한을 가지고 있지 않습니다."),
     ROOM_MEMBER_IS_NOT_PARTICIPANT("E05303", "방장 권한을 위임 받을 수 있는 참여자가 아닙니다."),
-    ROOM_MEMBER_IS_NOT_READY("E05101", "준비 상태가 아닌 멤버가 있습니다.");
+    ROOM_MEMBER_IS_NOT_READY("E05101", "준비 상태가 아닌 멤버가 있습니다."),
+    ROOM_MEMBER_CANNOT_ENTER("E05102", "방 참여 인원이 가득 찼습니다.");
 
     private final String errorCode;
     private final String errorMessage;
