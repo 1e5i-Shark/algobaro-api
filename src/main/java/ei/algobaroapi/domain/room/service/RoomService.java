5a0455523f79edc38c6daa22de1adebb84fb3d81
hyoguoo <hyoguoo@gmail.com>
2024-03-08 20:46:26 +0900

    mod #83: 동적쿼리 적용을 위한 조회 메서드 변경 및 PageResponse 객체 적용

--- src/main/java/ei/algobaroapi/domain/room/service/RoomService.java
+++ src/main/java/ei/algobaroapi/domain/room/service/RoomService.java
@@ -1,16 +1,17 @@
 package ei.algobaroapi.domain.room.service;
 
 import ei.algobaroapi.domain.member.domain.Member;
+import ei.algobaroapi.domain.room.domain.Room;
 import ei.algobaroapi.domain.room.dto.request.RoomCreateRequestDto;
 import ei.algobaroapi.domain.room.dto.request.RoomListRequestDto;
 import ei.algobaroapi.domain.room.dto.request.RoomUpdateRequestDto;
 import ei.algobaroapi.domain.room.dto.response.RoomDetailResponseDto;
 import ei.algobaroapi.domain.room.dto.response.RoomResponseDto;
-import java.util.List;
+import ei.algobaroapi.global.dto.PageResponse;
 
 public interface RoomService {
 
-    List<RoomResponseDto> getAllRooms(RoomListRequestDto roomListRequestDto);
+    PageResponse<Room, RoomResponseDto> getAllRooms(RoomListRequestDto roomListRequestDto);
 
     RoomDetailResponseDto createRoom(RoomCreateRequestDto roomCreateRequestDto, Member member);
 
