9d9c68327abba7c335d4b38124b9bddb0c50f3f0
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 22:44:38 +0900

    docs #87: password 요청 방식 변경(param -> body)

--- src/main/java/ei/algobaroapi/global/config/swaggerdoc/RoomMemberControllerDoc.java
+++ src/main/java/ei/algobaroapi/global/config/swaggerdoc/RoomMemberControllerDoc.java
@@ -3,13 +3,13 @@ package ei.algobaroapi.global.config.swaggerdoc;
 import ei.algobaroapi.domain.member.domain.Member;
 import ei.algobaroapi.domain.room_member.domain.RoomMember;
 import ei.algobaroapi.domain.room_member.dto.request.HostChangeRequestDto;
+import ei.algobaroapi.domain.room_member.dto.request.JoinRoomRequestDto;
 import ei.algobaroapi.domain.room_member.dto.response.RoomHostResponseDto;
 import ei.algobaroapi.domain.room_member.dto.response.RoomMemberResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "RoomMember", description = "방-회원 관련 API")
 @SuppressWarnings("unused")
@@ -20,7 +20,8 @@ public interface RoomMemberControllerDoc {
     @ApiResponse(responseCode = "E05302", description = "모집 중인 방이 아닙니다.")
     @ApiResponse(responseCode = "E05303", description = "입력한 비밀번호와 방 비밀번호가 일치하지 않습니다.")
     @ApiResponse(responseCode = "E05304", description = "방 참여 가능 인원이 가득 찼습니다.")
-    List<RoomMemberResponseDto> joinRoomByRoomId(Long roomId, String password, Member member);
+    List<RoomMemberResponseDto> joinRoomByRoomId(Long roomId, JoinRoomRequestDto joinRoomRequestDto,
+            Member member);
 
     @Operation(summary = "준비 상태 변경", description = "방 참여자의 준비 상태를 변경합니다. true -> false, false -> true")
     @ApiResponse(responseCode = "200", description = "준비 상태 변경에 성공하였습니다.")
