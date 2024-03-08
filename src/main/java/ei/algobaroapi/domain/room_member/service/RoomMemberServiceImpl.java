ed3aac1698afc59951495da6461432cb1306278a
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 22:12:39 +0900

    feat #87: 방 입장 시 검증 로직 추가
    - 모집 중인 방에 접근하는지 체크
    - 방 비밀번호가 일치하는지 체크
    - 참여할 수 있는 인원 수 인지 체크

--- src/main/java/ei/algobaroapi/domain/room_member/service/RoomMemberServiceImpl.java
+++ src/main/java/ei/algobaroapi/domain/room_member/service/RoomMemberServiceImpl.java
@@ -13,6 +13,7 @@ import ei.algobaroapi.domain.room_member.dto.response.RoomHostResponseDto;
 import ei.algobaroapi.domain.room_member.dto.response.RoomMemberResponseDto;
 import ei.algobaroapi.domain.room_member.exception.HostValidationException;
 import ei.algobaroapi.domain.room_member.exception.OrganizerValidationException;
+import ei.algobaroapi.domain.room_member.exception.RoomMemberNotEnterException;
 import ei.algobaroapi.domain.room_member.exception.RoomMemberNotFoundException;
 import ei.algobaroapi.domain.room_member.exception.RoomMemberNotReadyException;
 import ei.algobaroapi.domain.room_member.exception.common.RoomMemberErrorCode;
@@ -48,11 +49,12 @@ public class RoomMemberServiceImpl implements RoomMemberService {
 
     @Override
     @Transactional
-    public List<RoomMemberResponseDto> joinRoomByRoomId(Long roomId, Member member) {
+    public List<RoomMemberResponseDto> joinRoomByRoomId(Long roomId, String password,
+            Member member) {
         Room room = roomRepository.findById(roomId).orElseThrow(() -> RoomNotFoundException.of(
                 RoomErrorCode.ROOM_NOT_FOUND));
 
-        checkRoomIsAccessible(room);
+        validateConditionToJoinRoom(room, password);
 
         RoomMember roomMember = RoomMember.builder()
                 .room(room)
@@ -126,12 +128,34 @@ public class RoomMemberServiceImpl implements RoomMemberService {
         return findRoomMembers;
     }
 
-    private void checkRoomIsAccessible(Room room) {
-        int roomSize = roomMemberRepository.findByRoomId(room.getId()).size();
-        int roomLimit = room.getRoomLimit();
+    private void validateConditionToJoinRoom(Room room, String password) {
+        // 방에 참여할 수 있는지 확인 - 모집 중인지 체크
+        checkRoomIsRecruiting(room);
 
-        if (roomSize >= roomLimit) {
-            throw RoomMemberNotFoundException.of(RoomMemberErrorCode.ROOM_MEMBER_CANNOT_ENTER);
+        // 방에 참여할 수 있는지 확인 - 방 비밀번호 체크
+        checkRoomPassword(room, password);
+
+        // 방에 참여할 수 있는지 확인 - 인원 수 체크
+        checkRoomHeadCount(room);
+    }
+
+    private void checkRoomIsRecruiting(Room room) {
+        if (!room.isRecruiting()) {
+            throw RoomMemberNotEnterException.of(RoomMemberErrorCode.ROOM_MEMBER_CANNOT_ENTER_NOT_RECRUITING);
+        }
+    }
+
+    private void checkRoomPassword(Room room, String password) {
+        if (!room.passwordIsCorrect(password)) {
+            throw RoomMemberNotEnterException.of(RoomMemberErrorCode.ROOM_MEMBER_CANNOT_ENTER_PASSWORD);
+        }
+    }
+
+    private void checkRoomHeadCount(Room room) {
+        int roomSize = roomMemberRepository.findByRoomId(room.getId()).size();
+
+        if (room.isHeadCountFull(roomSize)) {
+            throw RoomMemberNotFoundException.of(RoomMemberErrorCode.ROOM_MEMBER_CANNOT_ENTER_HEADCOUNT);
         }
     }
 
