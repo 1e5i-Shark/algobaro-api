6791e3290530fc311f93a4637f6e0508c9aaae18
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 10:07:05 +0900

    feat #35: 방 인원 중 준비 하지 않는 인원에 대한 예외 추가

--- src/main/java/ei/algobaroapi/domain/room/exception/common/RoomErrorCode.java
+++ src/main/java/ei/algobaroapi/domain/room/exception/common/RoomErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RoomErrorCode {
 
-    ROOM_NOT_FOUND("E03301", "방을 찾지 못했습니다.");
+    ROOM_NOT_FOUND("E03301", "방을 찾지 못했습니다."),
+    ROOM_NOT_READY("E03101", "방 인원 중 준비 상태가 아닌 멤버가 있습니다.");
 
     private final String errorCode;
     private final String errorMessage;
