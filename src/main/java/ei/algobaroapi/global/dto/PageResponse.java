f07ba5879b587041d38480ab7f17240d6207cc28
hyoguoo <hyoguoo@gmail.com>
2024-03-08 15:12:01 +0900

    docs #83: swagger 문서화 작업

--- src/main/java/ei/algobaroapi/global/dto/PageResponse.java
+++ src/main/java/ei/algobaroapi/global/dto/PageResponse.java
@@ -1,15 +1,20 @@
 package ei.algobaroapi.global.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.function.Function;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 @Getter
+@Schema(description = "페이지네이션 응답")
 public class PageResponse<T, R> {
 
+    @Schema(description = "조회 결과 목록")
     private final List<R> content;
+    @Schema(description = "총 페이지 수", example = "5")
     private final int totalPages;
+    @Schema(description = "총 데이터 수", example = "59")
     private final long totalElements;
 
     private PageResponse(Page<T> pageResponse, Function<T, R> converter) {
