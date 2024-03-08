d21919e52a69a764f67bc51b367b5c064730fe9a
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 10:08:58 +0900

    feat #35: 문제 풀이 시작 시 발생 하는 로직 구현
    - roomShortUuid 통해 방 조회 및 roomId 조회
    - RoomMember List 조회
    - 해당 인원의 준비 상태 점검
    - SolveHistory 에 RoomMember 기초 정보 저장
    - 방 상태 변경 (모집 -> 실행)

--- src/main/java/ei/algobaroapi/domain/room/service/RoomServiceImpl.java
+++ src/main/java/ei/algobaroapi/domain/room/service/RoomServiceImpl.java
@@ -1,6 +1,7 @@
 package ei.algobaroapi.domain.room.service;
 
 import ei.algobaroapi.domain.member.domain.Member;
+import ei.algobaroapi.domain.member.service.MemberService;
 import ei.algobaroapi.domain.room.domain.Room;
 import ei.algobaroapi.domain.room.domain.RoomRepository;
 import ei.algobaroapi.domain.room.dto.request.RoomCreateRequestDto;
@@ -10,8 +11,12 @@ import ei.algobaroapi.domain.room.dto.response.RoomDetailResponseDto;
 import ei.algobaroapi.domain.room.dto.response.RoomResponseDto;
 import ei.algobaroapi.domain.room.exception.RoomNotFoundException;
 import ei.algobaroapi.domain.room.exception.common.RoomErrorCode;
+import ei.algobaroapi.domain.room_member.domain.RoomMember;
+import ei.algobaroapi.domain.room_member.domain.RoomMemberRepository;
 import ei.algobaroapi.domain.room_member.dto.response.RoomMemberResponseDto;
 import ei.algobaroapi.domain.room_member.service.RoomMemberService;
+import ei.algobaroapi.domain.solve.domain.SolveHistory;
+import ei.algobaroapi.domain.solve.domain.SolveHistoryRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -26,6 +31,10 @@ public class RoomServiceImpl implements RoomService {
 
     private final RoomRepository roomRepository;
     private final RoomMemberService roomMemberService;
+
+    private final MemberService memberService;
+    private final SolveHistoryRepository solveHistoryRepository;
+    private final RoomMemberRepository roomMemberRepository;
 
     @Override
     public List<RoomResponseDto> getAllRooms(RoomListRequestDto roomListRequestDto) {
@@ -70,8 +79,46 @@ public class RoomServiceImpl implements RoomService {
     }
 
     @Override
-    public void startCodingTest(Long roomId) {
-        // TODO: RoomStatus Running 으로 변환
+    @Transactional
+    public RoomDetailResponseDto startCodingTest(String roomShortUuid) {
+        // Room 찾기
+        Room room = roomRepository.findByRoomUuidStartingWith(roomShortUuid)
+                .orElseThrow(() -> RoomNotFoundException.of(RoomErrorCode.ROOM_NOT_FOUND));
+
+        Long roomId = room.getId();
+
+//        // RoomMember 찾기
+        List<RoomMember> findRoomMembers = roomMemberRepository.findByRoomId(roomId);
+
+        // RoomMember 들이 준비 상태 인지 확인
+        for (RoomMember roomMember : findRoomMembers) {
+            if (!checkRoomMemberIsReady(roomMember)) {
+                throw RoomNotFoundException.of(RoomErrorCode.ROOM_NOT_READY);
+            }
+        }
+
+        // SolveHistory 에 RoomMember 정보 저장
+        for (RoomMember roomMember : findRoomMembers) {
+            SolveHistory createSolveHistory = SolveHistory.builder()
+                    .member(memberService.getMemberByEmail(
+                            roomMember.getMember().getEmail().getEmail()))
+                    .roomUuid(roomShortUuid)
+                    .problemLink(room.getProblemLink())
+                    .build();
+
+            solveHistoryRepository.save(createSolveHistory);
+        }
+
+        // 방 상태 변경
+        room.updateRoomStatusRunning();
+
+        // 방 정보 반환
+        return RoomDetailResponseDto.of(room,
+                findRoomMembers.stream().map(RoomMemberResponseDto::of).toList());
+    }
+
+    private boolean checkRoomMemberIsReady(RoomMember roomMember) {
+        return roomMember.isReady();
     }
 
     private List<RoomMemberResponseDto> getRoomMembersByRoomId(Long roomId) {
