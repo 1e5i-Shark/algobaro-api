b24e73b70c5f248ef24379742d20b68f6cc1ca10
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 22:13:08 +0900

    feat #87: 모집 상태, 비밀번호, 방 인원 관련 에러 코드 추가

--- src/main/java/ei/algobaroapi/domain/room_member/exception/common/RoomMemberErrorCode.java
+++ src/main/java/ei/algobaroapi/domain/room_member/exception/common/RoomMemberErrorCode.java
@@ -11,7 +11,9 @@ public enum RoomMemberErrorCode {
     ROOM_MEMBER_IS_NOT_HOST("E05302", "방장 권한을 위임할 수 있는 권한을 가지고 있지 않습니다."),
     ROOM_MEMBER_IS_NOT_PARTICIPANT("E05303", "방장 권한을 위임 받을 수 있는 참여자가 아닙니다."),
     ROOM_MEMBER_IS_NOT_READY("E05101", "준비 상태가 아닌 멤버가 있습니다."),
-    ROOM_MEMBER_CANNOT_ENTER("E05102", "방 참여 인원이 가득 찼습니다.");
+    ROOM_MEMBER_CANNOT_ENTER_NOT_RECRUITING("E05102", "모집 중인 방이 아닙니다."),
+    ROOM_MEMBER_CANNOT_ENTER_PASSWORD("E05103", "입력한 비밀번호와 방 비밀번호가 일치하지 않습니다."),
+    ROOM_MEMBER_CANNOT_ENTER_HEADCOUNT("E05104", "방 참여 인원이 가득 찼습니다.");
 
     private final String errorCode;
     private final String errorMessage;
