b8b0c0ed2b85106acad8d29e8011b6a5430cde5b
hyoguoo <hyoguoo@gmail.com>
2024-03-08 20:43:57 +0900

    feat #83: 방 리스트 조회 querydsl 작성

--- src/main/java/ei/algobaroapi/domain/room/domain/RoomRepository.java
+++ src/main/java/ei/algobaroapi/domain/room/domain/RoomRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long>, RoomRepositoryQuery {
 
     @Query("SELECT r FROM Room r WHERE r.roomUuid LIKE CONCAT(:roomShortUuid, '%')")
     Optional<Room> findByRoomUuidStartingWith(String roomShortUuid); // TODO: 방 참여 기능 구현 후 쿼리 수정
