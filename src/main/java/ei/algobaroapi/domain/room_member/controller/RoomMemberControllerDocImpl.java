5584180d96e0a84e9835f9c4e628e653b4935bf8
kimnamgyu <lethargic.hood@gmail.com>
2024-03-08 16:54:18 +0900

    refactor #71: 사용하지 않는 코드 제거

--- src/main/java/ei/algobaroapi/domain/room_member/controller/RoomMemberControllerDocImpl.java
+++ src/main/java/ei/algobaroapi/domain/room_member/controller/RoomMemberControllerDocImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
