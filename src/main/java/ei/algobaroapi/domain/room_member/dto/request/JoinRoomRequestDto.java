daa5c17c0a5be2cee2d18489e5c3833662b01a89
kimnamgyu <lethargic.hood@gmail.com>
2024-03-09 19:32:23 +0900

    mod #74: 문서화 정보 추가 및 생성자 정보 추가

--- src/main/java/ei/algobaroapi/domain/room_member/dto/request/JoinRoomRequestDto.java
+++ src/main/java/ei/algobaroapi/domain/room_member/dto/request/JoinRoomRequestDto.java
@@ -1,11 +1,16 @@
 package ei.algobaroapi.domain.room_member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@Schema(description = "방 참여 요청 정보")
+@NoArgsConstructor
+@AllArgsConstructor
 public class JoinRoomRequestDto {
 
     @Schema(description = "방 비밀번호", example = "password1234")
