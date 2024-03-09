c64e469a7eaac7adc692b758ee6693abea89d179
kimnamgyu <lethargic.hood@gmail.com>
2024-03-09 20:15:10 +0900

    mod #74: 방장 자동 변경 요청 dto 방장 번호 dto 제거

--- src/main/java/ei/algobaroapi/domain/room_member/dto/request/HostAutoChangeRequestDto.java
+++ src/main/java/ei/algobaroapi/domain/room_member/dto/request/HostAutoChangeRequestDto.java
@@ -1,17 +1,18 @@
 package ei.algobaroapi.domain.room_member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @Schema(description = "방장 자동 변경 요청 정보")
+@NoArgsConstructor
+@AllArgsConstructor
 public class HostAutoChangeRequestDto {
 
     @Schema(description = "방 번호", example = "4")
     private Long roomId;
-
-    @Schema(description = "현재 방장 번호", example = "1")
-    private Long hostId;
 }
