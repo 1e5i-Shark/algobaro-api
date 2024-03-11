4a48ef53cffef3b7f60676d8c3998533fa8e6a09
kimnamgyu <lethargic.hood@gmail.com>
2024-03-11 12:15:47 +0900

    feat #99: 프로필 이미지 삭제 관련 API 추가

--- src/main/java/ei/algobaroapi/domain/member/controller/MemberController.java
+++ src/main/java/ei/algobaroapi/domain/member/controller/MemberController.java
@@ -10,10 +10,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -60,5 +62,13 @@ public class MemberController implements MemberControllerDoc {
             @RequestBody @Valid MemberPasswordUpdateRequest memberPasswordUpdateRequest
     ) {
         memberService.updateMemberPassword(member.getId(), memberPasswordUpdateRequest);
+    }
+
+    @Override
+    @DeleteMapping("/members/my/profile-image")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public void deleteMemberProfileImage(@AuthenticationPrincipal Member member,
+            @RequestParam String imageAddress) {
+        memberService.deleteMemberProfileImage(member.getId(), imageAddress);
     }
 }
